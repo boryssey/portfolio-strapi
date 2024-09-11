58e7854689f9ac723ff32acc427b63a0ebcd5dc3
Borys Melnyk <bor920@gmail.com>
2024-09-11 17:51:04 +0100

    feat: move from s3 to r2

--- config/plugins.ts
+++ config/plugins.ts
@@ -7,7 +7,7 @@ export default ({ env }) => ({
                 s3Options: {
                     accessKeyId: env('AWS_ACCESS_KEY_ID'),
                     secretAccessKey: env('AWS_ACCESS_SECRET'),
-                    region: env('AWS_REGION'),
+                    region: 'auto',
                     params: {
                         ACL: env('AWS_ACL', 'public-read'),
                         signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
